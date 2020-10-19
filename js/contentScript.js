507155e223f2aeb952e85e1a410840da990d32d1
Erlinis Quintana <erlinis.quintana@tourlane.com>
2020-10-19 02:43:37 +0200

    feat: set initial mode and add ability to remove darkMode

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,23 +1,38 @@
-
-chrome.runtime.onMessage.addListener(function(request){
-  darkify();
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
+  if (message.getInitialPageMode) {
+    sendResponse({ pageMode: getInitialPageMode() });
+  } else if (message.setPageMode) {
+    message.setPageMode === 'darkified' ? darkify() : lightify();
+    sendResponse({ pageMode: message.setPageMode });
+  }
 });
 
-function darkify(){
+
+const DARK_MODE_STYLE_HTML = `
+  html, img:not([src$=".svg"]), span[role='img'] {
+    transition: color 500ms, background-color 500ms;
+    filter: invert(95%);
+  }
+
+  video {
+    filter: invert(95%);
+  }
+`;
+
+let darkify = function (){
+  console.log("ContentScript:  darkify");
+
   let darkModeStyle = document.createElement('style');
   darkModeStyle.dataset.darkified = true;
-
-  darkModeStyle.innerHTML =`
-    html, img:not([src$=".svg"]), span[role='img'] {
-       transition: color 500ms, background-color 500ms;
-       filter: invert(95%);
-    }
-
-    video {
-      filter: invert(95%);
-    }
-  `;
+  darkModeStyle.innerHTML = DARK_MODE_STYLE_HTML;
 
   const firstScriptTag = document.querySelector('script');
   firstScriptTag.parentNode.insertBefore(darkModeStyle, firstScriptTag);
 };
+
+let lightify = function(){ darkStyle().remove(); }
+
+let darkStyle = function(){ return document.querySelector("style[data-darkified='true']") }
+
+let getInitialPageMode = function(){ return (darkStyle() !== null) ? 'darkified' : 'lightified'; }
+
--- js/popup.js
+++ js/popup.js
@@ -1,19 +1,58 @@
 'use strict';
 
+var pageMode = 'lightified';
 let goDark = document.getElementById("goDark");
 
-goDark.onclick = function(element) {
-  sendMessageToMainPage('darkify');
+goDark.onclick = function(event) {
+  console.log("Go dark: isCurrentPageDarkified: ", isCurrentPageDarkified());
+  isCurrentPageDarkified() ? lightify() : darkify ();
+}
 
-  goDark.textContent = "You are Darks!"
+
+let darkify = function(){
+  changePageMode({ setPageMode: 'darkified' });
+
+  goDark.textContent = "You're Darks!";
   goDark.classList.add('btn-pressed');
 }
 
-let sendMessageToMainPage = function(message){
-  // Query all tabs opened in the browser
-  chrome.tabs.query({ active: true, currentWindow: true},
-    function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, message)
-    }
-  );
+let lightify = function(){
+  changePageMode({ setPageMode: 'lightified' });
+
+  goDark.textContent = "Become Darks";
+  goDark.classList.remove('btn-pressed');
 }
+
+let isCurrentPageDarkified = () => { return (pageMode === 'darkified'); }
+
+let setPageMode = (mode) => { pageMode = mode; }
+
+let changePageMode = function(message){
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
+      setPageMode(response.pageMode);
+    });
+  });
+}
+
+let init = function(){
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, {getInitialPageMode: 'getInitialPageMode'},function(response) {
+
+      console.log(" ------- Opening popup -------- ");
+
+      console.log("1. setPageMode", response, response.pageMode);
+      setPageMode(response.pageMode);
+      console.log("2. isCurrentPageDarkified", isCurrentPageDarkified());
+
+      console.log(" ----------------- ");
+
+      if (isCurrentPageDarkified()) {
+        goDark.classList.add('btn-pressed');
+        goDark.textContent = "You're Darks!"
+      }
+    });
+  });
+}
+
+init();
